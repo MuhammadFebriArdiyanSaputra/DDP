7d90908382712f1374eaad0c2babfd552f31168f
M. Febri Ardiyan Saputra <112572361+MuhammadFebriArdiyanSaputra@users.noreply.github.com>
2022-09-08 11:06:01 +0700

    Add files via upload

---
+++ luas persegi dan keliling persegi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x, y;
+	cout << "Berapa sisi persegi= " << endl;
+	cin >> y;
+	x= y*y;
+	cout << " Luas persegi adalah " << x << endl;
+	return 0;
+	
+	
+	int sisi, keliling;
+	cout<<"Masukan sisi persegi : ";
+	cin>>sisi;
+	keliling = 4*sisi;
+	cout<<"Keliling persegi adalah "<< keliling ;
+	return 0;
+}
